1e409ef52390d6f0bbf31f0b0735d502457045ae
Jon <jon@laptop.local>
2018-12-10 14:05:04 -0500

    Fixing.

--- scripts/train.py
+++ scripts/train.py
@@ -123,7 +123,10 @@ def train_command(n_images,
                     use_transfer,
                     epoch_num,
                     critic_updates):
-
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    if not os.path.exists(weight_dir):
+        os.makedirs(weight_dir)
     return train_multiple_outputs(n_images,
                                     batch_size,
                                     input_dir,
