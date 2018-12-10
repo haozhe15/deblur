67ee980ae598028a6cbdd7381b12f537843348fd
Jon <jon@laptop.local>
2018-12-10 14:39:10 -0500

    Fix normalize_data_format import.

--- jon_dcc.sh
+++ jon_dcc.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH='deblur'
+export PYTHONPATH=$PYTHONPATH:~/deblur
 python ~/deblur/scripts/train.py --n_images=512 --input_dir '~/DeblurTrain/' --weights_dir 'weights/Deblur_nonlock' --generator_weights 'generator.h5'
 python ~/deblur/scripts/train.py --n_images=512 --input_dir '~/DeblurTrain/' --weights_dir 'weights/Deblur_lock' --generator_weights 'generator.h5' --use_transfer True
