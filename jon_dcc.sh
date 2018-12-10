ce12abc2867b5eb33ba55b09bbd4a1a519643208
Junhong Chen <jc634@dcc-slogin-01.oit.duke.edu>
2018-12-10 11:59:57 -0500

    jon_dcc.sh

---
+++ jon_dcc.sh
@@ -0,0 +1,3 @@
+
+ipython -- ~/deblur/scripts/train.py --n_images=512 --input_dir '~/DeblurTrain/' --weights_dir 'weights/Deblur_nonlock' --generator_weights 'generator.h5' 
+ipython -- ~/deblur/scripts/train.py --n_images=512 --input_dir '~/DeblurTrain/' --weights_dir 'weights/Deblur_lock' --generator_weights 'generator.h5' --use_transfer True
