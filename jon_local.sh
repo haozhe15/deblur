162ef564d669c6d0d78bb3281e0f1dcdaa46ab55
Jon <jon@laptop.local>
2018-12-10 14:27:09 -0500

    Fix module path.

--- jon_local.sh
+++ jon_local.sh
@@ -1,4 +1,3 @@
-
 export PYTHONPATH=`pwd`
 python scripts/train.py --n_images=512 --input_dir '../data/DeepVideo+CERTH' --weights_dir 'weights/Deblur_nonlock' --generator_weights 'generator.h5'
 python scripts/train.py --n_images=512 --input_dir '../data/DeepVideo+CERTH' --weights_dir 'weights/Deblur_lock' --generator_weights 'generator.h5' --use_transfer True
