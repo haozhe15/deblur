e2b0c6ec5ccc664ec5875aad51016195a30dbd16
Jon <jon@laptop.local>
2018-12-11 02:21:45 -0500

    Checkpoint

--- scripts/compare.py
+++ scripts/compare.py
@@ -7,13 +7,13 @@ from deblurgan.model import generator_model
 from deblurgan.utils import load_images, deprocess_image
 
 # Compare different models
-# input_dir = 'datasets/images/test', 'datasets/DIV2K/test' 
+# input_dir = 'datasets/images/test', 'datasets/DIV2K/test'
 # output_dir = 'myresults/compare'
 
 def compare(batch_size, input_dir, output_dir):
     data = load_images(input_dir, batch_size)
     y_test, x_test = data['B'], data['A']
-    weights = ['generator.h5', 'weights/DIV2K_1/generator_3_374.h5','weights/DIV2K_2/generator_3_507.h5']
+    weights = ['generator.h5', 'weights/local/Deblur_nonlock/generator_3_136.h5','weights/local/Deblur_lock/generator_3_210.h5']
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     generated = []
@@ -37,7 +37,7 @@ def compare(batch_size, input_dir, output_dir):
         output = np.concatenate((y, x, img_0, img_1, img_2), axis=1)
         im = Image.fromarray(output.astype(np.uint8))
         im.save(os.path.join(output_dir, 'results{}.png'.format(i)))
-        
+
         # store img seperately
         # im = Image.fromarray(img_0.astype(np.uint8))
         # im.save(os.path.join(output_dir, 'b0/results{}.png'.format(i)))
@@ -48,7 +48,7 @@ def compare(batch_size, input_dir, output_dir):
 
 
 @click.command()
-@click.option('--batch_size', default=4, help='Number of images to process')
+@click.option('--batch_size', default=100, help='Number of images to process')
 @click.option('--input_dir', required=True, help='Path to input images')
 @click.option('--output_dir', required=True, help='Path to output images')
 def compare_command(batch_size, input_dir, output_dir):
